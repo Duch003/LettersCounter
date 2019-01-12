b2504fd50cbcfb7a54219ce017454c4550e9cbdf
Duch003 <tomaszmankin003@gmail.com>
2019-01-12 05:31:08 +0000

    Problem with arcticles has been solved. Decoding left.

---
+++ ArcticleTitleDataModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LettersCounter
+{
+    public class ArcticleTitleDataModel
+    {
+        public string Title { get; set; } = "";
+        public Dictionary<char, int> Letters { get; set; }
+    
+
+        public ArcticleTitleDataModel(string title)
+        {
+            var cleanTitle = title.Replace("  ", " ").Replace("\r", "").Replace("\n", "");
+
+            Title = cleanTitle;
+            Letters = cleanTitle.GroupBy(letter => letter)
+                .OrderBy(letter => letter.Key)
+                .Where(grp => grp.Any())
+                .ToDictionary(grp => grp.Key, grp => grp.Count());
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,112 +18,212 @@ namespace LettersCounter
         {
             while (true)
             {
-                var document = DownloadWebsite();
-                var arcticlesHeaders = SelectHeaders(document);
-                ShowResults(arcticlesHeaders);
-                CreateStoreDocuments();
-                SaveData(arcticlesHeaders);
-                Thread.Sleep(1000 * 60 * 10);
-            }
-        }
+                var rawTitles = GatherArcticlesTitles();
+                var titles = PrepareData(rawTitles);
 
-        private static void CreateStoreDocuments()
-        {
-            for (var i = 1; i < 6; i++)
-            {
-                if (!File.Exists(i + ".txt"))
+                foreach (var title in titles)
                 {
-                    var file = File.Create(i + ".txt");
-                    file.Close();
-                }
-            }
-            var backFile = File.Create("backup.txt");
-            backFile.Close();
-        }
-
-        private static void SaveData(List<string> headers)
-        { 
-            for (int i = 4; i >= 1; i--)
-            {
-                var fs = File.OpenWrite(i + ".txt");
-                var sw = new StreamWriter(fs);
-
-                if (i == 1)
-                {
-                    foreach (var header in headers)
+                    Console.WriteLine(title.Title);
+                    foreach (var item in title.Letters)
                     {
-                        sw.WriteLine(header);
+                        Console.Write($"Letter {item.Key}: {item.Value}".PadRight(15));
                     }
+                    Console.WriteLine();
                 }
-                else
-                {
-                    var fsPrevious = File.OpenRead(i - 1 + ".txt");
-                    var sr = new StreamReader(fsPrevious);
-
-                    var information = sr.ReadToEnd();
-                    sw.WriteLine(information);
-
-                    fsPrevious.Close();
-                    sr.Close();
-                }
-                sw.Close();
-                fs.Close();
+                Console.ReadLine();
             }
+
+            
         }
 
-        private static void ShowResults(List<string> arcticlesHeaders)
+        private static List<ArcticleTitleDataModel> PrepareData(List<string> input)
         {
-            foreach (var title in arcticlesHeaders)
+            var output = new List<ArcticleTitleDataModel>();
+
+            foreach (var item in input)
             {
-                var cleanTitle = title.Replace(" ", "");
-
-                var output = cleanTitle.GroupBy(letter => letter)
-                    .OrderBy(letter => letter.Key)
-                    .Where(grp => grp.Any())
-                    .ToDictionary(grp => grp.Key, grp => grp.Count());
-
-                Console.WriteLine(title);
-
-                foreach (var record in output)
-                    Console.WriteLine($"{record.Key}: {record.Value}");
+                var arctileTitle = DecodeUnicode(DeocdeUTF8(item));
+                output.Add(new ArcticleTitleDataModel(arctileTitle));
             }
+
+            return output;
         }
 
-        private static HtmlDocument DownloadWebsite()
+        private static List<string> GatherArcticlesTitles()
+        {
+            var arcticles = new List<string>();
+            var arcticlesLeft = 20;
+            var page = 1;
+
+            do
+            {
+                var site = DownloadWebsite(page);
+                var titles = GatherArcticlesTitles(site, arcticlesLeft);
+
+                arcticles = MergeLists(arcticles, titles);
+                arcticlesLeft -= titles.Count;
+                page++;
+            } while (arcticlesLeft > 0);
+
+            return arcticles;
+        }
+
+        private static HtmlDocument DownloadWebsite(int page)
         {
             var client = new WebClient();
-            var document = new HtmlDocument();
+            var site = new HtmlDocument();
 
-            var content = client.DownloadString("https://www.finai.pl/blog");
-            document.LoadHtml(content);
+            site.LoadHtml(client.DownloadString($"https://www.finai.pl/blog?page={page}"));
 
-            return document;
+            return site;
         }
 
-        private static List<string> SelectHeaders(HtmlDocument document)
+        private static List<string> GatherArcticlesTitles(HtmlDocument site, int amount)
         {
-            var nodes = document.DocumentNode.SelectNodes("//h2").Take(20).ToList();
+            var output = new List<string>();
+            var nodes = site.DocumentNode.SelectNodes("//h2").Take(amount);
 
-            var arcticlesHeaders = new List<string>();
-
-            //There, because i don't know why C# html decoding doesnt work i add my own
-            //decoding conditions. I will try find out why decoding doesn't work later.
             foreach (var node in nodes)
             {
-                var title = WebUtility.HtmlDecode(node.InnerText);
-
-                title = title.Replace("Ĺ\u0083", "Ń")
-                    .Replace("Ĺ„", "ń")
-                    .Replace("Ĺ‚", "ł")
-                    .Replace("\r", "")
-                    .Replace("  ", " ")
-                    .Replace("\n", "");         
-
-                arcticlesHeaders.Add(title);
+                output.Add(node.InnerText);
             }
 
-            return arcticlesHeaders;
+            return output;
         }
+
+        private static string DeocdeUTF8(string input)
+        {
+            var utf8bytes = Encoding.UTF8.GetBytes(System.Text.RegularExpressions.Regex.Unescape(@input));
+            var decoder = Encoding.UTF8.GetDecoder();
+            var decodedLetters = new char[decoder.GetCharCount(utf8bytes, 0, utf8bytes.Length)];
+
+            decoder.GetChars(utf8bytes, 0, utf8bytes.Length, decodedLetters, 0);
+
+            var decoded = new string(decodedLetters);
+
+            decoded = WebUtility.HtmlDecode(@decoded);
+            decoded = System.Text.RegularExpressions.Regex.Unescape(@decoded);
+            return decoded;
+        }
+
+        private static string DecodeUnicode(string input)
+        {
+            var unicodeBytes = Encoding.Unicode.GetBytes(System.Text.RegularExpressions.Regex.Unescape(@input));
+            var decoder = Encoding.Unicode.GetDecoder();
+            var decodedLetters = new char[decoder.GetCharCount(unicodeBytes, 0, unicodeBytes.Length)];
+
+            decoder.GetChars(unicodeBytes, 0, unicodeBytes.Length, decodedLetters, 0);
+
+            var decoded = new string(decodedLetters);
+
+            decoded = WebUtility.HtmlDecode(@decoded);
+            decoded = System.Text.RegularExpressions.Regex.Unescape(@decoded);
+            return decoded;
+        }
+
+        private static List<string> MergeLists(List<string> list1, List<string> list2)
+        {
+            foreach (var item in list2)
+            {
+                list1.Add(item);
+            }
+
+            return list1;
+        }
+        //private static void CreateStoreDocuments()
+        //{
+        //    for (var i = 1; i < 6; i++)
+        //    {
+        //        if (!File.Exists(i + ".txt"))
+        //        {
+        //            var file = File.Create(i + ".txt");
+        //            file.Close();
+        //        }
+        //    }
+        //    var backFile = File.Create("backup.txt");
+        //    backFile.Close();
+        //}
+
+        //private static void SaveData(List<string> headers)
+        //{ 
+        //    for (int i = 4; i >= 1; i--)
+        //    {
+        //        var fs = File.OpenWrite(i + ".txt");
+        //        var sw = new StreamWriter(fs);
+
+        //        if (i == 1)
+        //        {
+        //            foreach (var header in headers)
+        //            {
+        //                sw.WriteLine(header);
+        //            }
+        //        }
+        //        else
+        //        {
+        //            var fsPrevious = File.OpenRead(i - 1 + ".txt");
+        //            var sr = new StreamReader(fsPrevious);
+
+        //            var information = sr.ReadToEnd();
+        //            sw.WriteLine(information);
+
+        //            fsPrevious.Close();
+        //            sr.Close();
+        //        }
+        //        sw.Close();
+        //        fs.Close();
+        //    }
+        //}
+
+        //private static void ShowResults(List<string> arcticlesHeaders)
+        //{
+        //    foreach (var title in arcticlesHeaders)
+        //    {
+        //        
+
+        //        Console.WriteLine(title);
+
+        //        foreach (var record in output)
+        //            Console.WriteLine($"{record.Key}: {record.Value}");
+        //    }
+        //}
+
+        //private static HtmlDocument DownloadWebsite(int page)
+        //{
+        //    var client = new WebClient();
+        //    var document = new HtmlDocument();
+
+        //    var content = client.DownloadString($"https://www.finai.pl/blog?page={ page }");
+        //    document.LoadHtml(content);
+
+        //    return document;
+        //}
+
+        
+
+        //private static List<string> SelectHeaders(HtmlDocument document)
+        //{
+        //    var nodes = document.DocumentNode.SelectNodes("//h2").Take(20).ToList();
+
+        //    var arcticlesHeaders = new List<string>();
+
+        //    //There, because i don't know why C# html decoding doesnt work i add my own
+        //    //decoding conditions. I will try find out why decoding doesn't work later.
+        //    foreach (var node in nodes)
+        //    {
+        //        var title = WebUtility.HtmlDecode(node.InnerText);
+
+        //        title = title.Replace("Ĺ\u0083", "Ń")
+        //            .Replace("Ĺ„", "ń")
+        //            .Replace("Ĺ‚", "ł")
+        //            .Replace("\r", "")
+        //            .Replace("  ", " ")
+        //            .Replace("\n", "");         
+
+        //        arcticlesHeaders.Add(title);
+        //    }
+
+        //    return arcticlesHeaders;
+        //}
 
         
     }
